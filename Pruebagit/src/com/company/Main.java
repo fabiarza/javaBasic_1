76c2edcc520d8f7846d951ea7c57450e18899f7e
fabiarza <fabianaarzamendia10@gmail.com>
2022-04-08 11:28:10 +0200

    commit#1:pruebagit

---
+++ Pruebagit/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        System.out.println("hello");
+        System.out.println("world");
+    }
+}
